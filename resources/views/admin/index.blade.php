b2681cb3b35efd426fbc8cb52e38c7c6df863d34
daniel ngwasi <damnngwasi@gmail.com>
2023-07-31 11:19:08 +0300

    dashbord initialization

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,7 +4,7 @@
 
     <div class="d-flex justify-content-between align-items-center flex-wrap grid-margin">
       <div>
-        <h4 class="mb-3 mb-md-0">Welcome to Dashboard</h4>
+        <h4 class="mb-3 mb-md-0">REAL ESTAT MANAGEMENT DASHBOARD</h4>
       </div>
       <div class="d-flex align-items-center flex-wrap text-nowrap">
         <div class="input-group flatpickr wd-200 me-2 mb-2 mb-md-0" id="dashboardDate">
@@ -134,43 +134,7 @@
       </div>
     </div> <!-- row -->
 
-    <div class="row">
-      <div class="col-12 col-xl-12 grid-margin stretch-card">
-        <div class="card overflow-hidden">
-          <div class="card-body">
-            <div class="d-flex justify-content-between align-items-baseline mb-4 mb-md-3">
-              <h6 class="card-title mb-0">Revenue</h6>
-              <div class="dropdown">
-                <a type="button" id="dropdownMenuButton3" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <i class="icon-lg text-muted pb-3px" data-feather="more-horizontal"></i>
-                </a>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton3">
-                  <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="eye" class="icon-sm me-2"></i> <span class="">View</span></a>
-                  <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="edit-2" class="icon-sm me-2"></i> <span class="">Edit</span></a>
-                  <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="trash" class="icon-sm me-2"></i> <span class="">Delete</span></a>
-                  <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="printer" class="icon-sm me-2"></i> <span class="">Print</span></a>
-                  <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="download" class="icon-sm me-2"></i> <span class="">Download</span></a>
-                </div>
-              </div>
-            </div>
-            <div class="row align-items-start">
-              <div class="col-md-7">
-                <p class="text-muted tx-13 mb-3 mb-md-0">Revenue is the income that a business has from its normal business activities, usually from the sale of goods and services to customers.</p>
-              </div>
-              <div class="col-md-5 d-flex justify-content-md-end">
-                <div class="btn-group mb-3 mb-md-0" role="group" aria-label="Basic example">
-                  <button type="button" class="btn btn-outline-primary">Today</button>
-                  <button type="button" class="btn btn-outline-primary d-none d-md-block">Week</button>
-                  <button type="button" class="btn btn-primary">Month</button>
-                  <button type="button" class="btn btn-outline-primary">Year</button>
-                </div>
-              </div>
-            </div>
-            <div id="revenueChart" ></div>
-          </div>
-        </div>
-      </div>
-    </div> <!-- row -->
+    
 
     <div class="row">
       <div class="col-lg-7 col-xl-8 grid-margin stretch-card">
@@ -196,47 +160,7 @@
           </div> 
         </div>
       </div>
-      <div class="col-lg-5 col-xl-4 grid-margin stretch-card">
-        <div class="card">
-          <div class="card-body">
-            <div class="d-flex justify-content-between align-items-baseline">
-              <h6 class="card-title mb-0">Cloud storage</h6>
-              <div class="dropdown mb-2">
-                <a type="button" id="dropdownMenuButton5" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <i class="icon-lg text-muted pb-3px" data-feather="more-horizontal"></i>
-                </a>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton5">
-                  <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="eye" class="icon-sm me-2"></i> <span class="">View</span></a>
-                  <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="edit-2" class="icon-sm me-2"></i> <span class="">Edit</span></a>
-                  <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="trash" class="icon-sm me-2"></i> <span class="">Delete</span></a>
-                  <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="printer" class="icon-sm me-2"></i> <span class="">Print</span></a>
-                  <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="download" class="icon-sm me-2"></i> <span class="">Download</span></a>
-                </div>
-              </div>
-            </div>
-            <div id="storageChart"></div>
-            <div class="row mb-3">
-              <div class="col-6 d-flex justify-content-end">
-                <div>
-                  <label class="d-flex align-items-center justify-content-end tx-10 text-uppercase fw-bolder">Total storage <span class="p-1 ms-1 rounded-circle bg-secondary"></span></label>
-                  <h5 class="fw-bolder mb-0 text-end">8TB</h5>
-                </div>
-              </div>
-              <div class="col-6">
-                <div>
-                  <label class="d-flex align-items-center tx-10 text-uppercase fw-bolder"><span class="p-1 me-1 rounded-circle bg-primary"></span> Used storage</label>
-                  <h5 class="fw-bolder mb-0">~5TB</h5>
-                </div>
-              </div>
-            </div>
-            <div class="d-grid">
-              <button class="btn btn-primary">Upgrade storage</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div> <!-- row -->
-
+  
     <div class="row">
       <div class="col-lg-5 col-xl-4 grid-margin grid-margin-xl-0 stretch-card">
         <div class="card">
